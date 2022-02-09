5c396279dc0fea913f67d8341d3f72cd4cb8461b
PK TOP <https://github.com/Naumkavg/TestBox.git>
2022-02-09 18:41:12 +0500

    TestBox Version2

--- src/test/java/box/ToolsQATest.java
+++ src/test/java/box/ToolsQATest.java
@@ -1,9 +1,10 @@
 package box;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
@@ -12,18 +13,25 @@ import static com.codeborne.selenide.Selenide.open;
 public class ToolsQATest {
 
     @BeforeAll
-    static void beforeAll(){
+    static void beforeAll() {
+
         Configuration.baseUrl = "https://demoqa.com";
     }
 
     @Test
     void successFillTest() {
         open("/automation-practice-form");
-        $(".=main-header").shouldHave(text("Practice Form"));
 
-                $("#firstName").setValue("Don");
-                $("#lastName").setValue("Korleone");
-                $("#userEmail").setValue("baron@boroda.com");
+        $(".main-header").shouldHave(text("Practice Form"));
+
+        $("#firstName").setValue("Don"); // имя
+        $("#lastName").setValue("Korleone"); // фамилия
+        $("#userEmail").setValue("baron@boroda.com");// почта
+              $("#gender-radio-1").setValue("Male"); // пол
+        $("#Mobile Number").setValue("123456789"); // номер телефона
+
     }
+
+
 
 }
